98353f5ef1ecd97e44507209da70117fbe0c1b39
Edgar Ezequiel Gonzalez Rodriguez <edgaregonzalez@gmail.com>
2017-05-04 21:56:19 -0300

    TP5 Ejercicio 3 #13

---
+++ tp5ej03.c
@@ -0,0 +1,37 @@
+/*
+Verificar si un número es par o impar, devolviendo 1 o 0 respectivamente.
+*/
+int verificar(int a);
+#include <stdio.h>
+
+int main(){
+
+	int numero=0;
+	int check;
+	
+
+	printf("Ingrese un numero \n");
+	scanf("%d",&numero);
+
+	check=verificar(numero);
+
+	if(check==0){
+		printf("El numero es impar \n");
+	}else{
+		printf("El numero es par \n");
+	}
+
+	return 0;
+
+}
+
+int verificar(int a){
+
+	if(a%2==0){
+
+		return 1;
+
+	}else{
+		return 0;
+	}
+}
