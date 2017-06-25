5001de61f264a654ec8d997acb07bb630b303afe
AnaLefinir <analefinir@gmail.com>
2017-06-25 20:42:40 -0300

    Ejercicio ocho terminado

--- TpsFundamentoAna/23-06/ejercicioenclase8.c
+++ TpsFundamentoAna/23-06/ejercicioenclase8.c
@@ -3,29 +3,67 @@ de forma ascendente. Ingresar un valor y ubicarlo por busqueda binaria.
 
 int main () 
 {   
-    int A[100];
+    int A[50];    
     int valor = -1;
-    int count = 0;
+    int i = 0;
+    int buscar = -1;
+    int flag = 1;
+    int aux = -1;
+    int izq = 0;
+    int der = 49;
+    int med = -1;
+    int pos = -1;
 
-    while (i<49) 
+    while (i < 50)
     {
-        printf("Ingrese un num");
+        printf("Ingrese un numero par");
         scanf("%d", &valor);
 
-        A[i] = valor;
-        i = i + 1;
-        
-        if (raizcuadrada(valor) == 1)
+        if (valor % 2 == 0)
         {
-            A[i] = 1;
+            A[i] = valor;
+            i = i + 1;
+        }
+    }
+
+    while (flag == 1)
+    {
+        flag = -1;
+
+        for (i = 0; i < 49; i = i + 1) 
+        {
+            if (A[i] > A[i + 1])
+            {
+                aux = A[i];
+                A[i] = A[i + 1];
+                A[i + 1] = aux;
+                flag = 1;
+            }
+        }
+    }
+
+    printf("Ingrese un valor a buscar");
+    scanf("%d", &buscar);
+
+    while (izq <= der && pos == -1)
+    {
+        med = (izq + der) / 2;
+
+        if (A[med] == buscar)
+        {
+            pos = med;
         }
         else
         {
-            A[i] = 0;
+            if (A[med] > buscar)
+            {
+                der = med - 1;
+            }
+            else 
+            {
+                izq = med + 1;
+            }
         }
-
-        i = i + 1;
-        
     }
 
     return 0;
