1d324783d4139883a164e3a8a4d232c0d4785b4b
AnaLefinir <analefinir@gmail.com>
2017-06-25 20:28:45 -0300

    Ejercicio siete de la clase 23/6 terminado y probado

--- TpsFundamentoAna/23-06/ejercicioenclase7.c
+++ TpsFundamentoAna/23-06/ejercicioenclase7.c
@@ -1,6 +1,7 @@
 Completar un arreglo con los numeros primos entre 1 y 100 y mostrarlos por pantalla.
 Utilizar una función para determinar si es primo.
 
+
 int esprimo ()
 {   
     int flag = 1;
@@ -21,15 +22,24 @@ int main ()
 {   
     int A[25];
     int valor = 0;
-    int i = 2;
+    int i = 0;
     int j = 0;
+    int estarepetido = -1;
 
     while (i < 25)
     {
         printf("Ingrese un numero primo");
         scanf("%d", &valor);
 
-        if ( valor > 1 && valor <= 100)
+        for (j = 0; j < i; j = j + 1) 
+        {
+            if (valor == A[j])
+            {
+                estarepetido = 1;
+            }
+        }
+
+        if ( valor > 1 && valor <= 100 && estarepetido == -1)
         {
             if (esprimo(valor) == 1)
             {
@@ -38,7 +48,10 @@ int main ()
                 printf("%d\n", valor);
             }
         }
+        estarepetido = -1;
     }
+
+    
 
     -- or --
 
